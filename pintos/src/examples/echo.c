8b8bd708fd0426cd098309188a64fff76c3d620b
f20230568 <f20230568@goa.bits-pilani.ac.in>
2025-08-31 23:39:17 +0530

    Edited both touch.c and echo.c

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <syscall.h>
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
   int i;
 
   for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
-  printf ("\n");
+    printf("%s ", argv[i]);
+  printf("\n");
 
   return EXIT_SUCCESS;
+}*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int
+main(int argc, char **argv)
+{
+  int i;
+  /* Print each argument separated by a space. */
+  for (i = 1; i < argc; i++) {
+    printf("%s", argv[i]);
+    /* Add a space after all except the last argument. */
+    if (i < argc - 1)
+      printf(" ");
+  }
+  printf("\n");
+  /* Always exit success for echo. */
+  return 0;
 }
