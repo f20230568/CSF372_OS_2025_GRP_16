0222af52395dbcb21a76b2f0b116d0e1e57f93fd
f20230568 <f20230568@goa.bits-pilani.ac.in>
2025-09-06 16:24:45 +0530

    Temporary commit

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,32 +1,17 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <syscall.h>
 
 int main(int argc, char **argv)
 {
   int i;
 
-  for (i = 0; i < argc; i++)
-    printf("%s ", argv[i]);
+  for (i = 1; i < argc; i++){
+    printf("%s", argv[i]);
+    if(i!=argc-1){
+      printf(" ");
+    }
+  }
   printf("\n");
 
   return EXIT_SUCCESS;
-}*/
-
-#include "stdio.h"
-#include "stdlib.h"
-
-int
-main(int argc, char **argv)
-{
-  int i;
-  /* Print each argument separated by a space. */
-  for (i = 1; i < argc; i++) {
-    printf("%s", argv[i]);
-    /* Add a space after all except the last argument. */
-    if (i < argc - 1)
-      printf(" ");
-  }
-  printf("\n");
-  /* Always exit success for echo. */
-  return 0;
 }
