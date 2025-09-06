7572d02638fd31e9e3bf34398bf90b085994612c
f20230568 <f20230568@goa.bits-pilani.ac.in>
2025-09-06 16:50:44 +0530

    Temporary commit

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <syscall.h>
 
 int main(int argc, char **argv)
 {
   int i;
-
-  for (i = 1; i < argc; i++){
-    printf("%s", argv[i]);
-    if(i!=argc-1){
-      printf(" ");
-    }
+  if(argc==1){
+    return EXIT_SUCCESS;
   }
-  printf("\n");
+
+  for (i = 1; i < argc; i++) {
+    if (argv[i][0] == '\'' && argv[i][strlen(argv[i]) - 1] == '\'') {
+      for (int j = 1; j < (int)strlen(argv[i]) - 1; j++) {
+        printf("%c", argv[i][j]);
+      }
+      if (i != argc - 1) printf("\n");
+      continue;
+    }
+    printf("%s", argv[i]);
+  }
 
   return EXIT_SUCCESS;
 }
