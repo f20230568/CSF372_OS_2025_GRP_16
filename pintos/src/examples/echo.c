c6bf3705d1aabaf76737fcfef4ac916b542a74a9
f20230568 <f20230568@goa.bits-pilani.ac.in>
2025-09-18 02:09:08 +0530

    Initial commit

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <syscall.h>
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
-  if(argc==1){
-    return EXIT_SUCCESS;
-  }
+  int i;
 
-  for(int i=1; i<argc; i++){
-    for(int j=0; j<strlen(argv[i]); j++){
-      if(argv[i][j]!='\''){
-        printf("%c", argv[i][j]);
-      }
-    }
-    printf(" ");
-  }
-  printf("\n");
+  for (i = 0; i < argc; i++)
+    printf ("%s ", argv[i]);
+  printf ("\n");
+
   return EXIT_SUCCESS;
 }
--- pintos/src/examples/touch.c
+++ pintos/src/examples/touch.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
-#include <syscall.h>
 
-int
-main (int argc, char **argv) {
-  if (argc < 2) {
-    return 1;
-  }
-  
-  int status = 0;
-
-  for (int i = 1; i < argc; i++) {
-    char *filename = argv[i];
-
-    int fd = open(filename);
-    if (fd >= 0) {
-      close(fd); 
-      status = 1; 
-      continue;   
-    }
-
-    if (!create(filename, 0)) {
-      printf("touch: failed to create %s\n", filename);
-    } else {
-      printf("%s: created\n", filename);
-    }
-  }
-  
-  return status;
+int main(int argc, char *argv[]) {
+    return 0;
 }
-
-
-
